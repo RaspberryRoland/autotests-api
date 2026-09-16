07ee5c0e555189a0eca3aa4a8bae88b72b1f071a
Valiev Robert <Robert.Valiev@infotecs.ru>
2026-09-16 08:58:11 +0500

    Знакомство с Faker. Практика работы с генерацией случайных данных

--- api_client_create_exercise.py
+++ api_client_create_exercise.py
@@ -1,5 +1,5 @@
-from clients.courses.courses_client import get_courses_client
 from clients.courses.course_schema import CreateCourseRequestSchema
+from clients.courses.courses_client import get_courses_client
 from clients.exercises.exercises_client import get_exercises_client
 from clients.exercises.exercises_schema import CreateExerciseRequestSchema
 from clients.files.files_client import get_files_client
@@ -8,16 +8,8 @@ from clients.private_http_builder import AuthenticationUserSchema
 from clients.users.public_users_client import get_public_users_client
 from clients.users.users_schema import CreateUserRequestSchema
 
-from tools.fakers import fake
-
 # Создаем пользователя
-create_user_request = CreateUserRequestSchema(
-    email=fake.email(),
-    password="string",
-    last_name="string",
-    first_name="string",
-    middle_name="string"
-)
+create_user_request = CreateUserRequestSchema()
 
 public_users_client = get_public_users_client()
 create_user_response = public_users_client.create_user(create_user_request)
@@ -29,11 +21,7 @@ authentication_user = AuthenticationUserSchema(
 )
 
 # Загружаем файл
-create_file_request = CreateFileRequestSchema(
-    filename="image.png",
-    directory="courses",
-    upload_file="./testdata/files/image.png"
-)
+create_file_request = CreateFileRequestSchema(upload_file="./testdata/files/image.png")
 
 files_client = get_files_client(authentication_user)
 create_file_response = files_client.create_file(create_file_request)
@@ -41,11 +29,6 @@ print(f"Create file data: {create_file_response}")
 
 # Создаем курс
 create_course_request = CreateCourseRequestSchema(
-    title="Python",
-    maxScore=100,
-    minScore=10,
-    description="Python API course",
-    estimatedTime="2 weeks",
     previewFileId=create_file_response.file.id,
     createdByUserId=create_user_response.user.id
 )
@@ -54,15 +37,7 @@ courses_client = get_courses_client(authentication_user)
 create_course_response = courses_client.create_course(create_course_request)
 print(f"Create course data: {create_course_response}")
 
-create_exercise_request = CreateExerciseRequestSchema(
-    title="Java",
-    courseId=create_course_response.course.id,
-    maxScore=0,
-    minScore=0,
-    orderIndex=0,
-    description="No description",
-    estimatedTime="To long"
-)
+create_exercise_request = CreateExerciseRequestSchema(courseId=create_course_response.course.id)
 
 exercises_client = get_exercises_client(authentication_user)
 create_exercise_response = exercises_client.create_exercise(
