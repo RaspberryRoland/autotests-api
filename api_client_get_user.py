3a8563adc50eeda34cc75ccc2743ba7aa3e6772d
Valiev Robert <Robert.Valiev@infotecs.ru>
2026-09-16 08:42:12 +0500

    Знакомство с Faker

--- api_client_get_user.py
+++ api_client_get_user.py
@@ -2,18 +2,12 @@ from clients.private_http_builder import AuthenticationUserSchema
 from clients.users.private_users_client import get_private_users_client
 from clients.users.public_users_client import get_public_users_client
 from clients.users.users_schema import CreateUserRequestSchema
-from tools.fakers import fake
 
 public_users_client = get_public_users_client()
 
-create_user_request = CreateUserRequestSchema(
-    email=fake.email(),
-    password="string",
-    last_name="string",
-    first_name="string",
-    middle_name="string"
-)
+create_user_request = CreateUserRequestSchema()
 create_user_response = public_users_client.create_user(create_user_request)
+print('Create user data:', create_user_response)
 
 authentication_user = AuthenticationUserSchema(
     email=create_user_request.email,
@@ -22,3 +16,4 @@ authentication_user = AuthenticationUserSchema(
 private_users_client = get_private_users_client(authentication_user)
 
 get_user_response = private_users_client.get_user(create_user_response.user.id)
+print('Get user data:', get_user_response)
